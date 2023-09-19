7019a4aef95b9b9d20ac3f8c10fd028c510bc3ab
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-09-19 23:24:53 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,12 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+shared_scripts {
+    'config.lua',
+	'locale.lua',
+	'languages/*.lua',
+}
+
 client_script {
     'client.lua',
     'warmenu.lua',
@@ -11,11 +17,7 @@ server_script {
     'server.lua',
 }
 
-shared_scripts {
-    'config.lua',
-	'locale.lua',
-	'languages/*.lua',
-}
+
 
 dependencies {
     'vorp_core',
